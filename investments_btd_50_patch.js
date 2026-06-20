a99819b27e97ce226c4278e8a78251fdea80657c
miemail1234-boop <miemail1234@gmail.com>
2026-06-20 11:56:36 +0200

    Load global restore entry update patch

--- investments_btd_50_patch.js
+++ investments_btd_50_patch.js
@@ -2,11 +2,11 @@
   'use strict';
   const scripts = [
     'investments_btd_correct_render.js?v=1',
-    'investments_entry_undo_patch.js?v=1'
+    'investments_entry_undo_patch.js?v=4'
   ];
   for (const src of scripts) {
     const s = document.createElement('script');
-    s.src = src + '&t=' + Date.now();
+    s.src = `${src}&t=${Date.now()}`;
     s.defer = true;
     document.head.appendChild(s);
   }
