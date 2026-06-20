fa9a475938c499af1900a48851eeda46f337135a
miemail1234-boop <miemail1234@gmail.com>
2026-06-20 13:30:20 +0200

    Load BTD full asset names patch

--- investments_btd_50_patch.js
+++ investments_btd_50_patch.js
@@ -9,6 +9,7 @@
   load('investments_overview_chart.js?v=4');
   load('investments_full_history_charts.js?v=2');
   load('investments_btd_correct_render.js?v=1');
+  load('investments_btd_full_names_patch.js?v=1');
   load('investments_entry_undo_patch.js?v=4');
   load('investments_entry_asset_name_patch.js?v=1');
 })();
