886db9dd4fb5fcca706684c7a07d9c2baf056350
miemail1234-boop <miemail1234@gmail.com>
2026-06-20 15:53:04 +0200

    Restore BTD raw results loader

---
+++ investments_btd_50_patch.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+  const s = document.createElement('script');
+  s.src = 'investments_btd_raw_results_patch.js?v=1';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
