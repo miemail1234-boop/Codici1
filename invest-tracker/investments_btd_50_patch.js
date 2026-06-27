2398fd2c0f53b12441eb311883e9476c178f0970
miemail1234-boop <miemail1234@gmail.com>
2026-06-27 11:25:48 +0200

    Load BTD button patch

--- invest-tracker/investments_btd_50_patch.js
+++ invest-tracker/investments_btd_50_patch.js
@@ -7,6 +7,7 @@
     document.head.appendChild(s);
   }
   load('btd_edge_scan_patch.js?v=1');
+  load('btd_update_button_patch.js?v=1');
   load('investments_overview_chart.js?v=4');
   load('investments_full_history_charts.js?v=2');
   load('investments_entry_undo_patch.js?v=4');
