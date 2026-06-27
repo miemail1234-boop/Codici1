26c183167aa1c571e049ff1e1fedbad8d934bc35
miemail1234-boop <miemail1234@gmail.com>
2026-06-27 14:25:41 +0200

    Load mobile safe section patch v7

--- invest-tracker/investments_btd_50_patch.js
+++ invest-tracker/investments_btd_50_patch.js
@@ -13,6 +13,6 @@
   load('investments_entry_undo_patch.js?v=4');
   load('investments_entry_asset_name_patch.js?v=1');
   load('investments_position_return_patch.js?v=2');
-  load('investments_sections_collapse_patch.js?v=6');
+  load('investments_sections_collapse_patch.js?v=7');
   load('investments_strategy_buttons_patch.js?v=2');
 })();
