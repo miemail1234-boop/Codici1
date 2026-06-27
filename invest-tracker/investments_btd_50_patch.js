3efc31484584d1b4f8ea8c6beed8dca9251f2f17
miemail1234-boop <miemail1234@gmail.com>
2026-06-27 12:36:32 +0200

    Load collapsible investment sections patch

--- invest-tracker/investments_btd_50_patch.js
+++ invest-tracker/investments_btd_50_patch.js
@@ -13,4 +13,5 @@
   load('investments_entry_undo_patch.js?v=4');
   load('investments_entry_asset_name_patch.js?v=1');
   load('investments_position_return_patch.js?v=2');
+  load('investments_sections_collapse_patch.js?v=1');
 })();
