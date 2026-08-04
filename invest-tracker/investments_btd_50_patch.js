1107ca57ef66f103746b8460998f947afae06b4a
miemail1234-boop <miemail1234@gmail.com>
2026-08-04 12:44:01 +0200

    Allinea loader ai grafici per cicli

--- invest-tracker/investments_btd_50_patch.js
+++ invest-tracker/investments_btd_50_patch.js
@@ -9,7 +9,7 @@
   load('btd_edge_scan_patch.js?v=1');
   load('btd_update_button_patch.js?v=1');
   load('investments_overview_chart.js?v=4');
-  load('investments_full_history_charts.js?v=2');
+  load('investments_full_history_charts.js?v=3');
   load('investments_entry_undo_patch.js?v=4');
   load('investments_entry_asset_name_patch.js?v=1');
   load('investments_position_return_patch.js?v=2');
