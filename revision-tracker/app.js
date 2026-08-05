c7167a33bfaafce8f652d4864acccdb461957caf
miemail1234-boop <miemail1234@gmail.com>
2026-08-05 18:25:13 +0200

    Load PDF support in Revision Tracker

--- revision-tracker/app.js
+++ revision-tracker/app.js
@@ -34,6 +34,7 @@ async function loadRevisionScripts() {
   await loadScript('revision_atlas_selection_fix.js?v=1');
   await loadScript('revision_editor_patch.js?v=1');
   await loadScript('revision_history_patch.js?v=1');
+  await loadScript('revision_pdf_patch.js?v=1');
   revisionScriptsLoaded = true;
 }
 
