50ceeb781870ef38e8c49773df66ea6b484eae56
miemail1234-boop <miemail1234@gmail.com>
2026-08-08 21:44:36 +0200

    Load BTD Score v2 patches

--- invest-tracker/investments_btd_50_patch.js
+++ invest-tracker/investments_btd_50_patch.js
@@ -6,9 +6,9 @@
     s.defer = true;
     document.head.appendChild(s);
   }
-  load('btd_edge_scan_patch.js?v=1');
+  load('btd_edge_scan_patch.js?v=2');
   load('btd_update_button_patch.js?v=1');
-  load('btd_news_patch.js?v=1');
+  load('btd_news_patch.js?v=2');
   load('investments_overview_chart.js?v=4');
   load('investments_entry_undo_patch.js?v=4');
   load('investments_entry_asset_name_patch.js?v=1');
